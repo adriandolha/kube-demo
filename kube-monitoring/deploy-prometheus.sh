f5fe40b73a58693e7791fa58ea2dae8d0c584f8b
adriandolha <adriandolha@yahoo.com>
2020-07-09 16:31:48 +0300

    [lorem-ipsum] - Enabled local deploy for monitoring, log aggregation and api gateway. Still have to fix kibana not enough resource.

--- kube-monitoring/deploy-prometheus.sh
+++ kube-monitoring/deploy-prometheus.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 kubectl apply -f monitoring-namespace.yaml
 kubectl apply -f prometheus-config.yaml
+kubectl delete -f prometheus-deployment.yaml --ignore-not-found
 kubectl apply -f prometheus-deployment.yaml
 kubectl apply -f prometheus-service.yaml
-kubectl apply -f node-exporter-daemonset.yml
+#kubectl apply -f node-exporter-daemonset.yml
 
-# custom metrics
-#kubectl apply -f custom-metrics-config-map.yaml
-#kubectl apply -f custom-metrics.yaml
