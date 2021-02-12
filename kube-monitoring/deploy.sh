2774579890cfcb97a15f52db3e0580bc80bdc5f6
adriandolha <adriandolha@yahoo.com>
2021-02-12 16:45:50 +0200

    [lorem-ipsum] - added authentication and authorization. updated infra scripts added monitoring metrics

--- kube-monitoring/deploy.sh
+++ kube-monitoring/deploy.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
+kubectl delete ns monitoring
+
 kubectl apply -f monitoring-namespace.yaml
 kubectl apply -f prometheus-config.yaml
+kubectl delete -f prometheus-deployment.yaml --ignore-not-found
 kubectl apply -f prometheus-deployment.yaml
 kubectl apply -f prometheus-service.yaml
 
-kubectl get services --namespace=monitoring
-kubectl get deployments --namespace=monitoring
-
-kubectl apply -f grafana-deployment.yaml
+helm install --name grafana stable/grafana -f grafana-values.yaml
+kubectl delete -f grafana-service.yaml
 kubectl apply -f grafana-service.yaml
 
-kubectl apply -f node-exporter-daemonset.yml
-
-# custom metrics
-#kubectl apply -f custom-metrics-config-map.yaml
-#kubectl apply -f custom-metrics.yaml
